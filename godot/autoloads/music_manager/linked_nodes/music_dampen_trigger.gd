2738d5c11e58f047cefaf642c7362af677a8b054
Zach Delgros <truegamer95@gmail.com>
2026-09-24 22:16:08 -0700

    Allow debug overlay use while paused
    Music dampen disables when the music dampen trigger within the pause menu is freed.

--- godot/autoloads/music_manager/linked_nodes/music_dampen_trigger.gd
+++ godot/autoloads/music_manager/linked_nodes/music_dampen_trigger.gd
@@ -3,6 +3,10 @@ extends Control
 
 func _ready() -> void:
 	visibility_changed.connect(_on_visibility_changed)
+	tree_exiting.connect(_on_tree_exiting)
 
 func _on_visibility_changed() -> void:
 	MusicManager.music_dampened = is_visible_in_tree()
+
+func _on_tree_exiting() -> void:
+	MusicManager.music_dampened = false # may not be a good idea to set here?
